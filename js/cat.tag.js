77d23a7bd907ebd56c131503636ab617ddc18c8e
Shuai Yuan <yazeishuai@gmail.com>
2018-02-01 10:41:57 -0500

    add files initially

---
+++ js/cat.tag.js
@@ -0,0 +1,16 @@
+
+<cat.tag>
+riot.tag2('p', 'Hi {message()}', '', '', function(opts) {
+});
+riot.tag2('script', '', '', '', function(opts) {
+  this.message = function() {
+    return 'there'
+  }.bind(this)
+});
+
+riot.tag2('style', '', '', '', function(opts) {
+  :scope p {
+    color: #000;
+  }
+});
+</cat.tag>
